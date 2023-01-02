c9fd481581182778d5e9fba080b1bf14a0b3063b
greyw0lv <noahcondotta@gmail.com>
2023-01-01 20:35:55 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("dec 1, 2022 1:0:0").getTime();
+var countDownDate = new Date("sept 2, 2022 1:0:0").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
